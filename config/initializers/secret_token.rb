60d10a574f853b99999a9c80655e6ac22d85eea4
ifnspifn <ifnspifn@ubuntu.(none)>
2012-06-24 17:17:32 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goto::Application.config.secret_token = 'cbd56df57199d1cad3ea266de1fc1a93448da7a94b00db5f6fde0e82185bb569c8bb43424085198576b9aba7acd55e23fb29656a2d98fddee74c4941139ce3d0'
